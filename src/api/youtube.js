33bd50c083c7c801705d57b370dc9618123003a4
mektigboy <mektigboy@protonmail.com>
2022-07-18 02:54:17 -0500

    Update files

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,1 +1,12 @@
+import axios from "axios";
+
 const KEY = "AIzaSyCUq1MZEDG7UwltKIZ-V-j87cb4uRZW4Fg";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY,
+  },
+});

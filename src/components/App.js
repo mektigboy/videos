8d9719e9e9225a8aca2cd6d9a732a67019344b44
mektigboy <mektigboy@protonmail.com>
2022-07-18 03:14:26 -0500

    Update files

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,18 @@ import SearchBar from "./SearchBar";
 import youtube from "../api/youtube";
 
 class App extends React.Component {
-  onTermSubmit = (term) => {
-    youtube.get("/search", { params: { q: term } });
-  };
+  state = { videos: [] };
+
+  async onTermSubmit(term) {
+    const response = await youtube.get("/search", { params: { q: term } });
+    this.setState({ videos: response.data.items });
+  }
 
   render() {
     return (
       <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <SearchBar onFormSubmit={(event) => this.onTermSubmit(event)} />I have{" "}
+        {this.state.videos.length} videos.
       </div>
     );
   }
